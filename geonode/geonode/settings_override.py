c6433b58aee78cb4570a449f7912a18e374feb3d
Henning Bredel <h.bredel@52north.org>
2024-09-06 10:50:26 +0200

    Update dev settings

--- geonode/geonode/settings_override.py
+++ geonode/geonode/settings_override.py
@@ -7,6 +7,7 @@
 import os
 import ast
 import sys
+import ast
 
 
 # sets defaults settings and from .env
@@ -22,18 +23,17 @@ SITE_HOST_NAME = "PlasticObs+"
 X_FRAME_OPTIONS = "SAMEORIGIN"
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None if DEBUG else "same-origin"
 
-
 # relax origins for geonode-mapstore-client development
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://localhost:8001",
-    "http://localhost:8081"
-] if DEBUG else ast.literal_eval(os.getenv("CSRF_TRUSTED_ORIGINS", "[]")) # noqa
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost",
-    "http://localhost:8001",
-    "http://localhost:8081"
-] if DEBUG else ast.literal_eval(os.getenv("CORS_ALLOWED_ORIGINS", "[]"))  # noqa
+CSRF_TRUSTED_ORIGINS = (
+    ["http://172.18.0.1:8001", "http://localhost:8081"]
+    if DEBUG
+    else ast.literal_eval(os.getenv("CSRF_TRUSTED_ORIGINS", "[]"))
+)
+CORS_ALLOWED_ORIGINS = (
+    ["http://172.18.0.1:8001", "http://localhost:8081"]
+    if DEBUG
+    else ast.literal_eval(os.getenv("CORS_ALLOWED_ORIGINS", "[]"))
+)
 
 
 STATIC_ROOT = "/mnt/volumes/statics/static/"
@@ -122,7 +122,9 @@ LOGGING = {
     },
 }
 
-LITTERASSESSMENT_MODEL_API = os.getenv("LITTERASSESSMENT_MODEL_API", "http://172.18.0.1:9000/v2/models/predict/")
+LITTERASSESSMENT_MODEL_API = os.getenv(
+    "LITTERASSESSMENT_MODEL_API", "http://172.18.0.1:5000"
+)
 
 INSTALLED_APPS += (
     "litterassessment",
