d95de32712e65bd37333e69d9f4a97ad9377f7a0
Henning Bredel <h.bredel@52north.org>
2025-02-27 16:31:11 +0100

    Revert group id confusion

--- geonode/apps/litterassessment/views.py
+++ geonode/apps/litterassessment/views.py
@@ -10,7 +10,6 @@ from django.http import (
 )
 
 from geonode.base.models import ResourceBase
-from django.contrib.auth.models import Group
 from geonode.base.auth import get_or_create_token
 from geonode.base.views import get_url_for_model
 from geonode.resource.models import ExecutionRequest
@@ -58,9 +57,7 @@ def _forward(method, path, headers={}, data=None):
 def _trigger_inference(user, path, payload, resource):
     
     inference = Inference.objects.create(payload=payload, resource=resource)
-    if "inferenceGroup" in payload:
-        group = Group.objects.filter(pk=payload["inferenceGroup"]).first()
-        inference.group = group.profile
+    inference.group_id = payload["inferenceGroup"] if "inferenceGroup" in payload else None
     inference.initiator = user
     inference.save()
 
