d4a3768ba759bf4102d598210086e8ab7a2b2551
Henning Bredel <h.bredel@52north.org>
2025-02-27 16:48:30 +0100

    Fix inference group
    We get the group .. NOT the profile. This is so that we can later
    udpate the Group via StoreMetadataHandler once merged on
    upstream.

--- geonode/apps/litterassessment/views.py
+++ geonode/apps/litterassessment/views.py
@@ -10,6 +10,7 @@ from django.http import (
 )
 
 from geonode.base.models import ResourceBase
+from geonode.groups.models import GroupProfile
 from geonode.base.auth import get_or_create_token
 from geonode.base.views import get_url_for_model
 from geonode.resource.models import ExecutionRequest
@@ -57,7 +58,9 @@ def _forward(method, path, headers={}, data=None):
 def _trigger_inference(user, path, payload, resource):
     
     inference = Inference.objects.create(payload=payload, resource=resource)
-    inference.group_id = payload["inferenceGroup"] if "inferenceGroup" in payload else None
+    if "inferenceGroup" in payload:
+        group = GroupProfile.objects.filter(group_id=payload["inferenceGroup"]).first()
+        inference.group = group
     inference.initiator = user
     inference.save()
 
